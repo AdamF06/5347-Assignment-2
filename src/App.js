2a75d3cf877f4f56a1e62074038e38ee4b9c706f
aaa <aaa@aaa.com>
2018-05-02 14:20:00 +1000

    Simple Login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {connect} from 'react-redux';
 import {ADD_USERNAME,DELETE_EVERYTHING} from './constants'
 
