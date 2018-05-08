670904f53d401dbd9770a2fff118600f3af94cb1
Scott <scott.guo@outlook.com>
2018-05-08 16:55:01 +1000

    Cleaning working tree

--- app/routes/survey.routes.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var controller = require('../controllers/survey.controller')
-var router = express.Router()
-
-router.get('/', controller.showMainPage)
-router.post('/inputname', controller.showResult)
-module.exports = router
