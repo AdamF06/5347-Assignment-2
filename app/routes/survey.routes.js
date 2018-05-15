610b91ae282c3f5bc408c7c5be6c6df45c45c44f
Scott <scott.guo@outlook.com>
2018-05-15 15:57:25 +1000

    Added some directory and trying to finish login

--- app/routes/survey.routes.js
+++ app/routes/survey.routes.js
@@ -1,9 +1,16 @@
 var express = require('express')
+var passport = require ('passport')
 var controller = require('../controllers/survey.controller')
+var loginController = require ('../controllers/login.controller')
 var router = express.Router()
 
 router.get('/', controller.showMainPage)
 router.post('/inputname', controller.showResult)
 router.get('/register',controller.showRegisterForm)
 router.post('/registerresult',controller.showRegisterResult)
+router.post ('/api/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: 'Invalid Username or Password.'
+}))
 module.exports = router
